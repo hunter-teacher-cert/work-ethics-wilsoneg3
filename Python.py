3fb862080b9c1577c6946cafe119e5eac05046f1
Eric Wilson <84479785+wilsoneg3@users.noreply.github.com>
2021-09-27 00:19:08 +0000

    added my initial understanding of lists, functions, and dictionaries as practiced on codeacademy

---
+++ Python.py
@@ -0,0 +1,14 @@
+#lists
+heights = [["Eric", 61], ["Tom", 70], ["Brady", 67], ["Sheila", 64], ["Tammy", 68]]
+ages = [["Carla", 15], ["Bozo", 16]]
+
+#functions
+def directions_to_timesSq():
+  print("Walk 4 mins to 34th St Herald Square train station.")
+  print("Take the Northbound N, Q, R, or W train 1 stop.")
+  print("Get off the Times Square 42nd Street stop.")
+  print("Take lots of pictures!")
+directions_to_timesSq()
+
+#dictionaries
+translations = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch"}
